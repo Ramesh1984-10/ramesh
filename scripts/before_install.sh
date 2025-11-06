1f207520a60b4c8e66f557c7d9cc5f1f231df52f
Ramesh1984-10 <59200344+Ramesh1984-10@users.noreply.github.com>
2025-11-06 15:27:24 +0530

    Fix rsync source path in before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,9 +2,8 @@
 set -e
 
 echo "Starting deployment using rsync..."
-sudo rm -rf /var/www/html/
 
 # Sync current directory contents to /var/www/html
-sudo rsync -av --delete /src /var/www/html/
+sudo rsync -av --delete ../src /var/www/html/
 
 echo "Deployment completed successfully!"
